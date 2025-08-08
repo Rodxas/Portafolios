50ea0b362ba48de7a29cf3e468fd02991726946f
Rodney Polanco <rodneypolanco81@gmail.com>
2025-08-08 16:28:02 -0400

    Sistema POS (Point Of Sale/Punto de venta)
    Sistema punto de venta remota
    
    Remote Point Of Sale System

---
+++ sistemaPOS/libreriaDll/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace libreriaDll
+{
+    public class Biblioteca
+    {
+        public static DataSet Herramientas(string cmd)
+        {                                              //Aquí debajo va la cadena de conexión del explorador de servidores
+                                                       //Se recomienda pooner una contraseña
+            SqlConnection conexion = new SqlConnection("Data Source=.;Initial Catalog=Sistema;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+            conexion.Open();
+
+            DataSet dll = new DataSet();
+            SqlDataAdapter dll1 = new SqlDataAdapter(cmd, conexion);
+
+            dll1.Fill(dll);
+
+            conexion.Close();
+
+            return dll;
+        }
+    }
+}
